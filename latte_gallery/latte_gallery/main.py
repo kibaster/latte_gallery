c713959772f6cbb42fa2fe9fd61619398f50c07c
ian <130588500+kibaster@users.noreply.github.com>
2024-11-04 22:18:28 +0400

    Add files via upload

--- latte_gallery/latte_gallery/main.py
+++ latte_gallery/latte_gallery/main.py
@@ -1,5 +1,8 @@
-from latte_gallery.setup import create_app
 import uvicorn
+
+from latte_gallery.core.setup import create_app
+
 app = create_app()
-if __name__ == '__main__':
-    uvicorn.run(app)
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080)
